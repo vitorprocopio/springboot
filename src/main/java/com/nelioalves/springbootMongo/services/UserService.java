2a87ae00cf59cf7caa193bbba01a50980307285e
vitorprocopio <vitor.nprocopio@gmail.com>
2020-09-07 15:09:41 -0300

    Exception

--- src/main/java/com/nelioalves/springbootMongo/services/UserService.java
+++ src/main/java/com/nelioalves/springbootMongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.nelioalves.springbootMongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.springbootMongo.domain.User;
 import com.nelioalves.springbootMongo.repository.UserRepository;
+import com.nelioalves.springbootMongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return repository.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
