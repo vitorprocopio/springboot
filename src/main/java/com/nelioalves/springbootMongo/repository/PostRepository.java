8fec1d7c1ae104584e38e996b7593dca381c4adb
vitorprocopio <vitor.nprocopio@gmail.com>
2020-09-10 16:52:52 -0300

    Entity Post com User aninhado

---
+++ src/main/java/com/nelioalves/springbootMongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.nelioalves.springbootMongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelioalves.springbootMongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
