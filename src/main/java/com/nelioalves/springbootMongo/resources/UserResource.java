3331069a15329e686b00869faa2d5ea8c77aafe8
vitorprocopio <vitor.nprocopio@gmail.com>
2020-09-11 10:57:43 -0300

    Retornando posts

--- src/main/java/com/nelioalves/springbootMongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/springbootMongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nelioalves.springbootMongo.domain.Post;
 import com.nelioalves.springbootMongo.domain.User;
 import com.nelioalves.springbootMongo.dto.UserDTO;
 import com.nelioalves.springbootMongo.services.UserService;
@@ -62,5 +63,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
